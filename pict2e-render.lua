6a1f29b1b901382d425135078d8773560d92787a
Michal Hoftich <michal.h21@gmail.com>
2016-11-07 00:04:27 +0100

    Added support for lines

--- pict2e-render.lua
+++ pict2e-render.lua
@@ -2,6 +2,7 @@ local function render(image)
   local lines =  {}
   local objects = image.objects or {}
   local function add_object(what)
+    print("add object", what)
     lines[#lines+1] = what
   end
   local function prepare_points(obj)
@@ -12,12 +13,17 @@ local function render(image)
     return points
   end
 
+  local function use_points(points)
+    return '(' .. table.concat(points, ')(') .. ')'
+  end
+
   for _,object in ipairs(objects) do
     local typ = object.type
     local points = prepare_points(object)
     if typ == "poly" then
-      add_object('\\polygon*' .. '(' .. table.concat(points, ')(') .. ')')
-      -- elseif typ == "line" then
+      add_object('\\polygon*' .. use_points(points) )
+    elseif typ == "line" then
+      add_object('\\Line' .. use_points(points))
     end
   end
   return table.concat(lines, "\n")
